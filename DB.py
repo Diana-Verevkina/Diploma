c48dfa03ccdae329390e12eec8ead590d6913159
Diana-Verevkina <diva2208@mail.ru>
2022-07-23 02:11:24 +0300

    first commit - skeleton

--- DB.py
+++ DB.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import json
-# import sqlite
+import os
+import sqlite3
 
 
-with open('project_book.json', encoding="utf-8") as file:
+with open(os.path.join('/Users/dianaverevkina/Diplom_project/Diploma/project_book.json')) as file:
     data = json.load(file)
 
 # Create A DataFrame From the JSON Data
 df = pd.DataFrame(data)
+
+conn = sqlite3.connect('data.db')
+cur = conn.cursor()
+df.to_sql("books", conn)
 
