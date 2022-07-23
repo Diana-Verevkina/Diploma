e66a8508f489a417e6614023d27fe62cc90c7fe3
Diana-Verevkina <diva2208@mail.ru>
2022-07-23 03:14:25 +0300

    create db

--- book_website/book_website/settings.py
+++ book_website/book_website/settings.py
@@ -77,10 +77,9 @@ WSGI_APPLICATION = 'book_website.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'data.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
--- book_website/books/models.py
+++ book_website/books/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=200)
+    author = models.CharField(max_length=50)
+    section = models.CharField(max_length=50)
+    age = models.CharField(max_length=5)
+    year = models.CharField(max_length=5)
+    pages = models.IntegerField()
+    rating = models.FloatField()
+    cove = models.TextField()
+    description = models.TextField()
+
